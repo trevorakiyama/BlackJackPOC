82dd6b8b38c6ab398df99c869f473b3ad3cf2c3c
Trevor Akiyama <trevor.akiyama@gmail.com>
2024-12-27 00:23:39 -0800

    Add shuffling to a card deck

--- BlackjackPOS/DeckOps/CardDeck.cs
+++ BlackjackPOS/DeckOps/CardDeck.cs
@@ -1,12 +1,54 @@
-﻿namespace BlackjackPOS.DeckOps;
+﻿using System.Text;
+
+namespace BlackjackPOS.DeckOps;
 
 public class CardDeck
 {
-
-    public CardDeck()
-    {
-        
-        // constructor
-    }
+    private readonly Card[] _cards = new Card[52];
+    private readonly Random _rng;
     
+    public CardDeck(Random rng)
+    {
+        this._rng = rng;
+         for (var i = 0; i < 52; i++) _cards[i] = new Card(i, i % 13, i / 13);
+    }
+
+    public void Shuffle()
+    {
+        int  n = _cards.Length;
+        while (n > 0)
+        {
+            n--;
+            var k = _rng.Next(n); // Generate a random index from 0 to n-1
+            // Swap the elements
+            Card value = _cards[k];
+            _cards[k] = _cards[n];
+            _cards[n] = value;
+            // Or even shorter
+            // (_cards[k], _cards[n]) = (_cards[n], _cards[k]);
+             
+        }
+    }
+
+
+    public new string ToString()
+    {
+        var builder = new StringBuilder();
+
+        foreach (var card in _cards)
+        {
+            builder.Append(card);
+            builder.Append("\n");
+        }
+
+        return builder.ToString();
+    }
+
+    public class Card(int id, int rank, int suit)
+    {
+        public override string ToString()
+        {
+            return id + "\tR:" + rank + "\tS:" + suit;
+        }
+    }
 }
--- BlackjackPOS/Program.cs
+++ BlackjackPOS/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BlackjackPOS;
+﻿using BlackjackPOS.DeckOps;
+
+namespace BlackjackPOS;
 
 public static class BlackjackPos
 {
@@ -6,7 +8,21 @@ public static class BlackjackPos
     static void Main()
     {  
 
-        Console.WriteLine("hello world");
+        Console.WriteLine("Initial Deck");
+
+        Random rnd = new Random();
+        
+        CardDeck deck = new CardDeck(rnd);
+        Console.WriteLine(deck.ToString());
+        
+        Console.WriteLine("Shuffled");
+        deck.Shuffle();
+        Console.WriteLine(deck.ToString());
+        
+        
+
+
+
     }
     
 }
