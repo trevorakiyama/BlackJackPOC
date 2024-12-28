5a3bff9dec3817a679b5e19ed5b28662b0679aaa
Trevor Akiyama <trevor.akiyama@gmail.com>
2024-12-28 15:42:54 -0800

    Add New CardDeckV2 version

--- BlackjackPOS/Program.cs
+++ BlackjackPOS/Program.cs
@@ -7,7 +7,12 @@ public static class BlackjackPos
     //static private CardDeck.Card card = null; 
     
     static void Main()
-    {  
+    {
+        if (false)
+        {
+            CardDeckV2.main2(null);
+        }
+
         Random rnd = new Random();
         
         CardDeck deck = new CardDeck(rnd);
