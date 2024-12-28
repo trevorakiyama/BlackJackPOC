c5bbb93eaf8f6c99a2d1a2f11798c8b5c724b33e
eliza <zeebee.akiyama@gmail.com>
2024-12-28 14:37:57 -0800

    Add Stand Method

--- BlackjackPOS/DeckOps/gameMethods.cs
+++ BlackjackPOS/DeckOps/gameMethods.cs
@@ -7,6 +7,8 @@ public class GameMethods
     public int Kitty;
     CardDeck _deck;
     public List<string> cards = new List<string>();
+    public List<string> dealerCards = new List<string>();
+    
 
     public GameMethods (CardDeck cardDeck)
     {
@@ -24,6 +26,10 @@ public class GameMethods
         var dealer = _deck.Draw();
         var dealer2 = _deck.Draw();
         DealerScore = dealer.Rank + dealer2.Rank; 
+        dealerCards.Add(_deck.SuitRankConvert(dealer));
+        dealerCards.Add(_deck.SuitRankConvert(dealer2));
+        
+        
         var card1 = _deck.Draw();
         var card2 = _deck.Draw();
         cards.Add(_deck.SuitRankConvert(card1));
@@ -39,45 +45,53 @@ public class GameMethods
         }
         Console.WriteLine();
         Console.WriteLine("HIT or STAND?");
-        
-        if (Console.ReadLine() == "HIT" || Console.ReadLine() == "hit")
+        var line = Console.ReadLine();
+        if (line == "HIT" || line == "hit")
         {
             Hit();
-        } else if (Console.ReadLine() == "STAND" || Console.ReadLine() == "stand")
+        } else if (line == "STAND" || line == "stand")
         {
-            
+            Stand();
         }
         
     }
 
     public void Hit()
     {
+        Console.WriteLine("Dealer's Cards: ");
+        Console.WriteLine(dealerCards[0] + " | ?");
+        
+        
         var card1 = _deck.Draw();
         cards.Add(_deck.SuitRankConvert(card1));
 
-        PlayerScore = card1.Rank + PlayerScore + 1;
+        PlayerScore = card1.Rank + PlayerScore;
+        
+        Console.WriteLine("Your cards are:");
+        for (int i = 0; i < cards.Count; i++)
+        {
+            Console.Write(cards[i] + " ");
+        }
         if (PlayerScore <= 21)
         {
-                  Console.WriteLine("Your cards are:");
-                  for (int i = 0; i < cards.Count; i++)
-                  {
-                      Console.Write(cards[i] + " ");
-                  }
-                  Console.WriteLine();
-                  Console.WriteLine("HIT or BET?");
+
+            Console.WriteLine(); 
+            Console.WriteLine("HIT or STAND?");
+            var line = Console.ReadLine();
                   
-                  if (Console.ReadLine() == "HIT" || Console.ReadLine() == "hit")
-                  {
-                      Hit();
-                  } else if (Console.ReadLine() == "STAND" || Console.ReadLine() == "stand")
-                  {
-                      
-                  }
+            if (line == "HIT" || line == "hit")
+            { 
+                Hit();
+            } else if (line == "STAND" || line == "stand")
+            { 
+                Stand();
+            }
   
             
         }
         else
         {
+            Console.WriteLine();
             Console.WriteLine("Bust!");
             Console.WriteLine("Try again? y/n");
             if (Console.ReadLine() == "y" || Console.ReadLine() == "yes")
@@ -99,6 +113,31 @@ public class GameMethods
 
     }
 
+    public void Stand()
+    {
+        var dealerCard = _deck.Draw();
+        dealerCards.Add(_deck.SuitRankConvert(dealerCard));
+        DealerScore = DealerScore + dealerCard.Rank;
+        
+        Console.WriteLine("Dealer's Cards: ");
+        for (int i = 0; i < dealerCards.Count; i++)
+        {
+            Console.Write(dealerCards[i] + " ");
+        }
+        
+        if (PlayerScore <= DealerScore)
+        {
+            Console.WriteLine();
+            Console.WriteLine("You Lose! Your Score is:" + PlayerScore + " The Dealers Score is:" + DealerScore);
+        }
+        else if (PlayerScore >= DealerScore)
+        {
+            Console.WriteLine();
+            Console.WriteLine("You Win! Your Score is:" + PlayerScore + " The Dealers Score is:" + DealerScore);
+        }
+
+
+}
 
     
 }
